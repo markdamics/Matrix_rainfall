f95cff4682adef987c69fa14e34ab69cb8105d4c
markdamics <mdamics@geomant.com>
2024-12-20 21:38:09 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,42 @@ use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegrou
 use crossterm::{cursor, queue, terminal};
 use rand::Rng;
 
-const CHARS: [&str; 2] = ["1", "0"];
+const CHARS: [char; 2] = ['1', '0'];
 
 #[derive(Clone)]
 struct Column {
     length: u16,
-    charachter_color: Color,
+    character_color: Color,
+    characters: Vec<char>,
+    vector_index: usize
 }
 
 impl Column {
-    fn new(length: u16, charachter_color: Color) -> Self {
+    fn new(length: u16, character_color: Color) -> Self {
         Self {
             length,
-            charachter_color
+            character_color,
+            characters: vec![' '; length as usize],
+            vector_index: 0
         }
     }
 
     fn init(&self, stdout: &mut std::io::Stdout) -> Result<()> {
-        for _ in 0..self.length {
-            let random_char = get_random_char();
-            queue!(stdout, Print(random_char.to_string()))?;
+        for (i, char) in self.characters.iter().enumerate() {
+            queue!(stdout, SetForegroundColor(self.character_color))?;
+            queue!(stdout, Print(char)).context("initialize with empty characters")?;
         }
         Ok(())
     }
 
-    fn update(&self) -> Result<()> {
+    fn update(&mut self) -> Result<()> {
+        self.characters[self.vector_index] = get_random_char();
+        self.vector_index += 1;
+
+        if self.vector_index >= self.length as usize {
+            self.vector_index = 0;
+        }
+        //Why does it "moving" in rows?
         Ok(())
     }
 }
@@ -38,37 +49,40 @@ impl Column {
 struct Matrix {
     width: u16,
     height: u16,
-    charachter_color: Color,
+    character_color: Color,
     background_color: Color,
     columns: Vec<Column>,
 }
 
 impl Matrix {
-    fn new(width: u16, height: u16, charachter_color: Color, background_color: Color) -> Self {
+    fn new(width: u16, height: u16, character_color: Color, background_color: Color) -> Self {
         Self {
             width,
             height,
-            charachter_color,
+            character_color,
             background_color,
-            columns: vec![Column::new(height, charachter_color); width as usize]
+            columns: vec![Column::new(height, character_color); width as usize]
         }
     }
 
     fn init(&self, stdout: &mut std::io::Stdout) -> Result<()> {
+        queue!(stdout, cursor::MoveTo(0, 0))?;
         queue!(stdout, SetBackgroundColor(self.background_color))?;
-        queue!(stdout, SetForegroundColor(self.charachter_color))?;
 
         for col in &self.columns {
             col.init(stdout)?;
         }
 
         queue!(stdout, ResetColor)?;
-        queue!(stdout, cursor::MoveTo(0, 0))?;
+        queue!(stdout, cursor::Show)?;
 
         Ok(())
     }
 
-    fn update(&self) -> Result<()> {
+    fn update(&mut self) -> Result<()> {
+        for col in &mut self.columns {
+            col.update()?;
+        }
         Ok(())
     }
 }
@@ -77,26 +91,27 @@ fn main() -> Result<()> {
 
     let mut stdout = stdout();
     let (width, height) = terminal::size().context("Get terminal size")?;
-    let charachter_color = Color::Rgb { r: 0, g: 255, b: 0 };
+    let character_color = Color::Rgb { r: 0, g: 255, b: 0 };
     let background_color = Color::Rgb { r: 0, g: 0, b: 0 };
 
     queue!(stdout, cursor::MoveTo(0, 0))?;
     queue!(stdout, SetBackgroundColor(background_color))?;
-    queue!(stdout, SetForegroundColor(charachter_color))?;
+    queue!(stdout, SetForegroundColor(character_color))?;
 
-    let matrix = Matrix::new(width, height, charachter_color, background_color);
+    let mut matrix = Matrix::new(width, height, character_color, background_color);
 
     loop{
         matrix.init(&mut stdout).context("Initialize matrix")?;
+        matrix.update().context("Update matrix")?;
 
         stdout.flush()?;
-        sleep(Duration::from_millis(1000));
+        sleep(Duration::from_millis(500));
     }
     Ok(())
 }
 
-fn get_random_char() -> String {
-    let mut rng = rand::thread_rng();
-    let random_index = rng.gen_range(0..CHARS.len());
-    CHARS[random_index].to_string()
+fn get_random_char() -> char {
+    let mut rng = rand::rng();
+    let random_index = rng.random_range(0..CHARS.len());
+    CHARS[random_index]
 }
